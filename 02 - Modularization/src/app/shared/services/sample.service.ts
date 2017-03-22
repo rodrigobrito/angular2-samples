5091dbc6d713c9aa0364442b43dff89a4ab04dad
Rodrigo Brito <Rodrigo Brito>
2017-03-22 00:17:23 -0300

    Complementando...

--- 02 - Modularization/src/app/shared/services/sample.service.ts
+++ 02 - Modularization/src/app/shared/services/sample.service.ts
@@ -1,13 +1,15 @@
 export class SampleService {
     static counter = 0;
+    static instances = 0;
     id = 0;
 
-    constructor() {   
-        console.log("Shared service initialized.");   
+    constructor() { 
+        SampleService.instances++;
+        console.log("Shared service initialized." + SampleService.instances);   
     }
 
     increment() : void {
         this.id = ++SampleService.counter;
-        console.log(`Sample with id ${this.id}`);
+        console.log(`Sample with id ${this.id} and instance ${SampleService.instances}`);
     }
  }
